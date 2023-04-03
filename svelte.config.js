5d6eaee310ebe88b513362011e47f7ed7f3ec586
Jordi Martinoy i Font <jordi.martyn1@gmail.com>
2023-04-03 21:20:16 +0200

    Fixed: Changed vite base path to /weather-app

--- svelte.config.js
+++ svelte.config.js
@@ -3,6 +3,5 @@ import { vitePreprocess } from '@sveltejs/vite-plugin-svelte'
 export default {
   // Consult https://svelte.dev/docs#compile-time-svelte-preprocess
   // for more information about preprocessors
-  base: '/weather-app/',
   preprocess: vitePreprocess(),
 }
